296031ef29cffda794c4b0fe7766d4c46a47a3eb
Sarvesh207 <sarveshgaynar@gmail.com>
2024-01-26 23:05:55 +0530

    done with custom class errors and apiresponse

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import app, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 
 dotenv.config({
     path:"./.env"
 })
 
+const PORT = process.env.PORT || 8000
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`⚙️  Server is running on port : ${PORT}`)
+    })
+})
+.catch( (err) => {
+    console.log('MONGODB connection failed !!!', err)
+}
+    
+)
 
 
 
